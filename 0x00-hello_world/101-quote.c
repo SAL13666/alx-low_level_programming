c6fa02a836bb5eecb9bd4122e46aa5fdba22cc8a
SAL13666 <heso482485@gmail.com>
2023-02-17 04:19:19 -0800

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 1 (Success)
+/*
+ * p
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
