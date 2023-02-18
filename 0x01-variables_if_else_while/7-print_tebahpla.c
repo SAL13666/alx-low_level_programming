de65b0cbe6c48da59615f16c7ddfda31fbf0be8a
SAL13666 <heso482485@gmail.com>
2023-02-17 18:46:19 -0800

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/*
+*hi
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+char x;
+for (x = 'z'; x >= 'a'; x--)
+putchar(x);
+
+putchar('\n');
+
+return (0);
+}
