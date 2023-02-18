1a0821f4ac7afd74c2b4e56cd8569d13d7d159f6
SAL13666 <heso482485@gmail.com>
2023-02-17 18:00:31 -0800

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+*hi
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+char x;
+for (x = 'a';x >= 'z';x++)
+putchar(x);
+putchar("\n");
+return (0);
+}
