b3649bf723eeb2eddef305a003d8c22de695d9b7
SAL13666 <heso482485@gmail.com>
2023-02-17 18:39:40 -0800

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+*hi
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%d", x);
+
+printf("\n");
+
+return (0);
+}
