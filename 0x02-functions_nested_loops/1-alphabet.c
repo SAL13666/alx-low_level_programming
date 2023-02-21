445b8578d099f1bc41559a6ac66be04b5e2770bf
SAL13666 <heso482485@gmail.com>
2023-02-21 09:21:28 -0800

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+char a;
+for (a ='a' ; a <= 'z'; a++)
+{
+_putchar(a)
+}
+_putchar('\n')
+return (0);
+}
