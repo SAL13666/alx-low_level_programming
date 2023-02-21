7d5a340b96b3ee15611a69f6ce5c59b84d5b0002
SAL13666 <heso482485@gmail.com>
2023-02-21 10:55:06 -0800

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
+#include <ctype.h>
 /**
  *  _isalpha- Prints 10 times the alphabet, in lowercase.
  */
-int _isalpha(int c)
+int _islower(int c)
 {
-if (c >= 'a' || c <= 'Z')
+if (islower(c))
 return (1);
 else
 return (2);
