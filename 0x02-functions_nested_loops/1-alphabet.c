cb0603760ff1c30aed3dee87a3214b68cb723f18
SAL13666 <heso482485@gmail.com>
2023-02-21 09:24:17 -0800

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-int main(void)
+
+void print_alphabet(void)
 {
 char a;
 for (a ='a' ; a <= 'z'; a++)
-{
-_putchar(a)
-}
-_putchar('\n')
+
+_putchar(a);
+
+_putchar('\n');
 return (0);
 }
