0034fac63b34cff6509ba90e6dfb2710859fa53f
SAL13666 <heso482485@gmail.com>
2023-02-21 09:27:49 -0800

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,13 @@
  *
  * Return: Always 0.
  */
-
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
 char a;
-for (a ='a' ; a <= 'z'; a++)
+for (a = 'a' ; a <= 'z'; a++)
 
 _putchar(a);
 
