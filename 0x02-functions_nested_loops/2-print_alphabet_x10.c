91f49fb131ef6ac2fcb46d20281e1b4508946362
SAL13666 <heso482485@gmail.com>
2023-02-21 09:43:31 -0800

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - 
- * Prints all possible combinations of single-digit numbers.
+ * print_alphabet_x10 -  Prints all possible combinations of single-digit numbers.
  */
 void print_alphabet_x10(void)
 {
 char a;
-int n = 0;
-while (n < 10)
+int n;
+while (n=0; < 10; n++)
 {
 for (a = 'a' ; a <= 'z'; a++)
 
 _putchar(a);
 
 _putchar('\n');
-n++;
 }
 }
