889406a234bdf85aba197d07cb81a01c1ffefc19
SAL13666 <heso482485@gmail.com>
2023-02-24 07:38:19 -0800

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
