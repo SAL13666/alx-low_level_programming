4b706de1d6b664e563e467bbf16472a1f4724749
SAL13666 <heso482485@gmail.com>
2023-02-24 07:42:23 -0800

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,13 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
-int _isupper(int c)
+int _isupper(int x)
 {
-if (c >= 65 && c <= 90)
-return (1);
-else
-return (0);
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
 }
+
