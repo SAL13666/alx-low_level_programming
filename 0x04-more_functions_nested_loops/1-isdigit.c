46c85f8fe1b8720a691192dc9a61a4916435f98c
SAL13666 <heso482485@gmail.com>
2023-02-24 07:45:34 -0800

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
