1fba9cd0b1c20f0774a1f70e775402842be53d35
SAL13666 <heso482485@gmail.com>
2023-02-27 08:36:01 -0800

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
   * print_rev- print a string reversed followed by new line
-  * @s: the string 
+  * @s: the string
   * Return: void
   */
 void print_rev(char *s)
 {
 int counter = 0;
-while(s[counter] != '\0')
+while (s[counter] != '\0')
 {
 counter++;
 }
-for(counter -= 1; counter >= 0; counter--)
+for (counter -= 1; counter >= 0; counter--)
 {
 _putchar(s[counter]);
 }
