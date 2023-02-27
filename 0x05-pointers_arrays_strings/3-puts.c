f502f8b1d29b4424f6f247081d70dea59a307f8f
SAL13666 <heso482485@gmail.com>
2023-02-27 07:54:50 -0800

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _puts- print a string followed by new line
+  * @str: the string 
+  * Return: void
+  */
+void _puts(char *str)
+{
+int counter=0;
+while (*str != '\0')
+{
+_putchar(str)
+str++;
+}
+_putchar('\n');
+}
