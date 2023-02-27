b0964954235ba09f9148467831f85e7ebbd74636
SAL13666 <heso482485@gmail.com>
2023-02-27 10:07:33 -0800

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,28 +6,23 @@
   */
 void rev_string(char *s)
 {
-int counter = 0;
-
-while (s[counter] != '\0')
-{
-counter++;
-}
-
-int l, i;
+int l = 0, i;
 char *begin_ptr, *end_ptr, ch;
-l = counter;
+while(s[l] != '\0')
+{
+l++;
+}
 
 begin_ptr = s;
 end_ptr = s + l - 1;
 
 for (i = 0; i < (l - 1) / 2; i++) {
-
 ch = *end_ptr;
 *end_ptr = *begin_ptr;
 *begin_ptr = ch;
-
 begin_ptr++;
 end_ptr--;
 }
 }
+
 
