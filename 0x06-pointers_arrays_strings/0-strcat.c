972b262c4ba8a3b09360fbe1d56bd912801fa178
SAL13666 <heso482485@gmail.com>
2023-03-01 09:37:25 -0800

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * _strcat- print a string reversed followed by new line
+  * @str: the string
+  * @dest: the string
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+int dlen = 0;
+while (dest[dlen] != '\0')
+{
+dlen++;
+}
+for (int i = 0; src[i] != 0; i++)
+{
+dest[dlen] = src[i];
+dlen++;
+}
+dest[dlen] = '\0';
+return (dest);
+}
+
+
+
+
