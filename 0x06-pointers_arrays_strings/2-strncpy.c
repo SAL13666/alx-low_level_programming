0cc9bfc8303c9c3d3dca5bd3141f7bd403a597bf
SAL13666 <heso482485@gmail.com>
2023-03-02 07:34:25 -0800

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * _strncpy- print a string reversed followed by new line
+  * @src: the string
+  * @dest: the string
+  * @n: the number
+  * Return: dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+while (i < n && src[i])
+{
+dest[i] = src[i];
+i++;
+}
+
+while (i < n)
+{
+dest[i] = '\0';
+a++;
+}
+return (dest);
+}
+
+
