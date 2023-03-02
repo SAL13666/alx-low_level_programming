d74a9cf56738e6e72e15da35dcadc5829d3fa55c
SAL13666 <heso482485@gmail.com>
2023-03-02 07:54:32 -0800

    a

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,17 +7,20 @@
   */
 void reverse_array(int *a, int n)
 {
-int tmp, i = 0, m = 0;
-for (m = 1; a < n; m++)
-{
-m++;
-}
-while (i < n / 2)
-{
-tmp = a[i];
-a[i] = a[m];
-a[m] = tmp;
-m--;
-i++;
-}
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
 }
