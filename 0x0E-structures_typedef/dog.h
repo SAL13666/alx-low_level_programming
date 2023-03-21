e6cb175e9009d0fd707b87c5f7263126ff737cef
SAL13666 <heso482485@gmail.com>
2023-03-21 11:42:51 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG
+#define DOG
+/**
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
+
+
+
+
+
+
+#endif
