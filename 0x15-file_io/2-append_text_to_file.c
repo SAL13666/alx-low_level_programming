0f1b8af3e17b1608c8918399153f19f33e471075
SAL13666 <heso482485@gmail.com>
2023-04-11 09:03:10 -0700

    a

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+size_t strln(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * append_text_to_file - 0
+ * @filename: 1
+ * @text_content: 2
+ *
+ * Return: 3
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t len;
+
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+len = write(fd, text_content, strln(text_content));
+close(fd);
+if (len == -1)
+return (-1);
+return (1);
+}
